8c83b37eb11967fad943e8993e4a347d753bf13b
Игорь Сиротин <sirotin@phystech.edu>
2017-10-18 21:44:31 +0300

    Исправил ошибку, из-за которой при отвалившемся эфире из пхп вываливалась какафня.

--- build/php/icecast-current-track.php
+++ build/php/icecast-current-track.php
@@ -1,5 +1,8 @@
 <?php
 $status_json = @file_get_contents('http://radio.mipt.ru:8410./status-json.xsl');
 $status = json_decode($status_json);
-$now = $status->{'icestats'}->{'source'}->{'title'};
+
+$now = "";
+if (property_exists($status->{'icestats'}, "source"))
+    $now = $status->{'icestats'}->{'source'}->{'title'};
 print_r($now);
--- build/php/icecast-listeners-number.php
+++ build/php/icecast-listeners-number.php
@@ -1,5 +1,9 @@
 <?php
 $status_json = @file_get_contents('http://radio.mipt.ru:8410/status-json.xsl');
 $status = json_decode($status_json);
-$now = $status->{'icestats'}->{'source'}->{'listeners'};
+
+$now = 0;
+if (property_exists($status->{'icestats'}, "source"))
+    $now = $status->{'icestats'}->{'source'}->{'listeners'};
+
 print_r($now);
