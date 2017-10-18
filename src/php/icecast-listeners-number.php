4cf3eee949f171a6af98cf9a52a897bdba4f6ce8
Игорь Сиротин <sirotin@phystech.edu>
2017-10-18 21:26:32 +0300

    Добавлена страница "hype-level.html" с отображением текущего количества слушателей.

---
+++ src/php/icecast-listeners-number.php
@@ -0,0 +1,5 @@
+﻿<?php
+$status_json = @file_get_contents('http://radio.mipt.ru:8410/status-json.xsl');
+$status = json_decode($status_json);
+$now = $status->{'icestats'}->{'source'}->{'listeners'};
+print_r($now);
