9f94e35f49b2ee9807f0ea53c74af1b789ca546c
Игорь Сиротин <sirotin@phystech.edu>
2017-11-26 16:31:04 +0300

    Убрал с главной инфу про Эфир Века.

--- src/js/century-broadcast-script.js
+++ src/js/century-broadcast-script.js
@@ -28,35 +28,6 @@ function swapElementClasses(e, from, to) {
 
 var angle = 0;
 
-// Set the date we're counting down to
-var countDownDate = new Date("Nov 25, 2017 12:00:00").getTime();
-
-// Update the count down every 1 second
-var countdown = setInterval(function() {
-
-	// Get todays date and time
-	var now = new Date().getTime();
-
-	// Find the distance between now an the count down date
-	var distance = countDownDate - now;
-
-	// Time calculations for days, hours, minutes and seconds
-	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-	// Display the result in the element with id="demo"
-	document.getElementById("timer").innerHTML = "До начала Эфира Века: <b>" + hours + "</b>ч <b>"
-		+ minutes + "</b>м <b>" + seconds + "</b>с";
-
-	// If the count down is finished, write some text
-	if (distance < 0) {
-		clearInterval(countdown);
-		document.getElementById("timer").innerHTML = "<b>⇨ Расписание эфира здесь ⇦</b>";
-	}
-}, 1000);
-
 function startPlanetRotation() {
 	var planet = $('#mipt-planet');
 
