84c3afda1e4b4d380e20eaaff866b72335eda7e3
Daniil Okhlopkov <danokhlopkov@gmail.com>
2018-04-10 18:20:23 +0300

    Update icecast-current-track.php

--- src/php/icecast-current-track.php
+++ src/php/icecast-current-track.php
@@ -1,8 +1,10 @@
 ﻿<?php
+header('Access-Control-Allow-Origin: *');
 $status_json = @file_get_contents('http://radio.mipt.ru:8410./status-json.xsl');
 $status = json_decode($status_json);
 
 $now = "";
 if (property_exists($status->{'icestats'}, "source"))
     $now = $status->{'icestats'}->{'source'}->{'title'};
+header('Access-Control-Allow-Origin: *');
 print_r($now);
